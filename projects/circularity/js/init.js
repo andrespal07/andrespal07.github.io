1fedef07cad8cf54585909cfffeb99770f6d4ef0
andrespal122 Palencia <andrespal122@gmail.com>
2023-11-29 14:28:45 +0000

    ~

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -30,7 +30,11 @@ var init = function (window) {
 
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
